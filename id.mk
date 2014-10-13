1b0b86497ce9c65216be2b3e01388198f341f6bf
Thomas Fossati <tfossati@tfossati-mbp.local>
2014-10-13 19:20:55 +0100

    check for undef'd ID var

--- id.mk
+++ id.mk
@@ -1,5 +1,9 @@
 XML2RFC := /usr/local/bin/xml2rfc
 
+ifndef ID
+  $(error ID must be the basename of the I-D xml file)
+endif
+
 all: $(ID).xml ; $(XML2RFC) --text --html $<
 
 clean: ; $(RM) $(ID).html $(ID).txt
